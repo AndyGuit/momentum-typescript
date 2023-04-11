74c56f85fa4d5a720671663fb1d68c01334e6024
AndyGuit <totalslash@msn.com>
2023-04-11 20:19:16 +0300

    feat: add interfaces for background image responses

--- src/_apiLinks.ts
+++ src/_apiLinks.ts
@@ -4,20 +4,32 @@ export const getGithubImg = (imgNum: string, tag: string) => {
 
 const unsplashKey = '7ujidxk950M1vbghIqHgUXaODWIEEKDKilnv7eeiWJU';
 
+interface UnsplashRes {
+  urls: {
+    regular: string;
+  };
+}
+
 export const getUnsplashImg = async (tag: string) => {
   const url = `https://api.unsplash.com/photos/random?query=${tag}&client_id=${unsplashKey}`;
   const res = await fetch(url);
-  const { urls } = await res.json();
-  return urls.regular;
+  const data: UnsplashRes = await res.json();
+  return data.urls.regular;
 };
 
 const flickrKey = '9ff3c0b8944f4f287cb74c77ca1ed047';
 
+interface FlickrRes {
+  photos: {
+    photo: {
+      url_l: string;
+    }[];
+  };
+}
+
 export const getFlickrImg = async (imgNum: string, tag: string) => {
   const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrKey}&tags=${tag}&tag_mode=any&extras=url_l&format=json&nojsoncallback=1`;
   const res = await fetch(url);
-  const { photos } = await res.json();
-  console.log(imgNum, tag);
-  console.log(photos);
-  return photos.photo[imgNum].url_l;
+  const data: FlickrRes = await res.json();
+  return data.photos.photo[+imgNum].url_l;
 };
